f806f7f12076836368b3e86321e416d59ad33fc2
msp3021 <131592979+msp3021@users.noreply.github.com>
2024-07-08 19:33:59 +0000

    Deploying to gh-pages from @ msp3021/goit-js-hw-10@5bb7ed7e253d74dc54b40dd61e3ffc4f99fe3c3e 🚀

---
+++ assets/02-snackbar-2bf3cea7.js
@@ -0,0 +1,2 @@
+import{i}from"./vendor-77e16229.js";document.getElementById("promise-form").addEventListener("submit",function(e){e.preventDefault();const s=parseInt(e.target.elements.delay.value),r=e.target.elements.state.value;m(s,r).then(t=>{i.success({title:"Success",message:`✅ Fulfilled promise in ${t}ms`})}).catch(t=>{i.error({title:"Error",message:`❌ Rejected promise in ${t}ms`})})});function m(e,s){return new Promise((r,t)=>{setTimeout(()=>{s==="fulfilled"?r(e):t(e)},e)})}
+//# sourceMappingURL=02-snackbar-2bf3cea7.js.map
